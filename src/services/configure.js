24d2a1d48dc826cd36b8ba6c14303d54f87e2b7f
lpinkas <lpinkas@gmail.com>
2020-12-03 21:05:07 -0300

    Cambios para deploy heroku

--- src/services/configure.js
+++ src/services/configure.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 console.log(process.env, process.env.REACT_APP_NOT_SECRET_CODE);
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://azure/app/31231231/api",
 });
 
 const token = localStorage.getItem('jwt');
